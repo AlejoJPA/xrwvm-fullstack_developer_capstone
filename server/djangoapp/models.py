7cb2156f9e2288ac149dc4107cdfde0ea7624e6e
Alejo <alejojpa2@gmail.com>
2025-05-07 08:10:13 -0400

    deleting typos in models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,3 @@
-# Uncomment the following imports before adding the Model code
-
  from django.db import models
  from django.utils.timezone import now
  from django.core.validators import MaxValueValidator, MinValueValidator
@@ -46,4 +44,4 @@ class CarModel(models.Model):
     created_at = models.DateTimeField(default=now)
 
     def __str__(self):
-        return f'Car name: {self.name} - Car type: {self.car_type} – Date: {seld.year}'
+        return f'Car name: {self.name} - Car type: {self.car_type} – Date: {self.year}'
