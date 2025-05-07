603f6c3bcea91e2aeeacaf49cb967a6d03f4974d
Alejo <alejojpa2@gmail.com>
2025-05-07 08:08:02 -0400

    Creating models: CarMake and CarModel

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,49 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+ from django.db import models
+ from django.utils.timezone import now
+ from django.core.validators import MaxValueValidator, MinValueValidator
 
+#Car make model
+class CarMake(models.Model):
 
-# Create your models here.
+    #car maker name an description
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return self.Name
 
+#Car Model model
+class CarModel(models.Model):
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    # Car type choices
+    SEDAN = 'SEDAN'
+    SUV = 'SUV'
+    WAGON = 'WAGON'
+    HATCHBACK = 'HATCHBACK'
+    COUPE = 'COUPE'
+    TRUCK = 'TRUCK'
+
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (HATCHBACK, 'Hatchback'),
+        (COUPE, 'Coupe'),
+        (TRUCK, 'Truck'),
+    ]
+
+    #Many-To-One relationship to CarMake model
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+
+    #car model name, type(car_type), release year(year)
+    name = models.CharField(max_length=30)    
+    car_type = models.CharField(max_length=10, choices=CAR_TYPES, default=SEDAN)
+    year = models.IntegerField(validators=[MinValueValidator(2015), MaxValueValidator(2023)])
+
+    #Report creation date (created_at)
+    created_at = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f'Car name: {self.name} - Car type: {self.car_type} – Date: {seld.year}'
