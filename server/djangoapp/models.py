0fcfc338da91973a39c433405405d89026b69f93
Alejo <alejojpa2@gmail.com>
2025-05-31 05:14:58 -0400

    flake8 fixs for models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,19 +2,19 @@ from django.db import models
 from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-#Car make model
-class CarMake(models.Model):
 
-    #car maker name an description
+# Car make model
+class CarMake(models.Model):
+    # Car maker name and description
     name = models.CharField(max_length=30)
-    description = models.TextField() #description = models.CharField(max_length=100)
+    description = models.TextField()
 
     def __str__(self):
         return self.name
 
-#Car Model model
-class CarModel(models.Model):
 
+# Car model
+class CarModel(models.Model):
     # Car type choices
     SEDAN = 'SEDAN'
     SUV = 'SUV'
@@ -32,20 +32,26 @@ class CarModel(models.Model):
         (TRUCK, 'Truck'),
     ]
 
-    #Many-To-One relationship to CarMake model
+    # Many-To-One relationship to CarMake model
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
 
-    #car model name, type(car_type), release year(year)
+    # Car model name, type, and release year
     name = models.CharField(max_length=100)
-    car_type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-        validators=[MinValueValidator(2015),
-                     MaxValueValidator(2023)
-        ])
+    car_type = models.CharField(
+        max_length=10,
+        choices=CAR_TYPES,
+        default=SUV,
+    )
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MinValueValidator(2015),
+            MaxValueValidator(2023),
+        ],
+    )
 
-    #Report creation date (created_at)
+    # Report creation date
     created_at = models.DateTimeField(default=now)
 
     def __str__(self):
-        #return f'Car name: {self.name} - Car type: {self.car_type} – Release Date: {self.year}'
         return self.name
