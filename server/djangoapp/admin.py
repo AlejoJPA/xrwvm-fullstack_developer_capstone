d2458e9a0fe39a30a9d22721d28105075e71b103
Alejo <alejojpa2@gmail.com>
2025-05-31 03:06:10 -0400

    pylint 10/10, flake8 correcting space between classes

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -5,12 +5,12 @@ from django.contrib import admin
 from .models import CarMake, CarModel
 
 
-
 # CarModelInline class
 class CarModelInline(admin.TabularInline):
     '''InLine for CarModel in CarMake admin'''
     model = CarModel
     extra = 1
+
 
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
@@ -19,12 +19,14 @@ class CarModelAdmin(admin.ModelAdmin):
     list_filter = ('car_type', 'year', 'car_make')
     search_fields = ('name',)
 
+
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     '''Admin configuration for CarMake'''
     inlines = [CarModelInline]
     list_display = ('name', 'description')
     search_fields = ('name',)
+
 
 # Register models here
 admin.site.register(CarMake, CarMakeAdmin)
