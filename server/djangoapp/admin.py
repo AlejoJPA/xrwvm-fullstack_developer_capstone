38a557407c267795a9519e6ba280cdd20a8622d3
Alejo <alejojpa2@gmail.com>
2025-05-31 02:51:23 -0400

    pylint 10/10 for djangoapp/admin.py _Inserting linespace 3

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,32 +1,34 @@
 """Admin configuration for Django models."""
 
+
 from django.contrib import admin
 from .models import CarMake, CarModel
+
 
 
 # Register your models here.
 # CarModelInline class
 class CarModelInline(admin.TabularInline):
-    
     '''InLine for CarModel in CarMake admin'''
     model = CarModel
     extra = 1
 
+
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
-
     '''Admin configuration for CarModel'''
     list_display = ('name', 'car_make', 'car_type', 'year')
     list_filter = ('car_type', 'year', 'car_make')
     search_fields = ('name',)
 
+
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
-
     '''Admin configuration for CarMake'''
     inlines = [CarModelInline]
     list_display = ('name', 'description')
     search_fields = ('name',)
+
 
 # Register models here
 admin.site.register(CarMake, CarMakeAdmin)
